6282a6ff2d96fd7e5a81e4760991a41c30a11282
Johdeath <Johannes.arzt@gmx.net>
2018-11-13 11:30:59 +0100

    add assertJ to maven add aggregateNetworktraffic test

--- src/test/java/edu/hm/jarzt/RecordsTest.java
+++ src/test/java/edu/hm/jarzt/RecordsTest.java
@@ -1,10 +1,25 @@
 package edu.hm.jarzt;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class RecordsTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void aggregatesNetworkTraffic() {
+        File file = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "trafficPattern01 - Kopie.csv");
+        Records records = new Records(file);
+        List<Integer> periods = records.aggregatesNetworkTraffic(200,2);
+
+        assertThat(periods)
+               .hasSize(9)
+               .containsExactly(720,453,1718,1025,883,279,1104,441,479);
+
     }
 }
