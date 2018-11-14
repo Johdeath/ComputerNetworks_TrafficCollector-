8009cb8640677da338da112b4a834e76061efa3e
Johdeath <Johannes.arzt@gmx.net>
2018-11-14 10:39:11 +0100

    add new test and rename testfiles

--- src/test/java/edu/hm/jarzt/RecordsTest.java
+++ src/test/java/edu/hm/jarzt/RecordsTest.java
@@ -11,15 +11,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RecordsTest {
 
+
+
     @Test
-    void aggregatesNetworkTraffic() {
-        File file = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "trafficPattern01 - Kopie.csv");
+    void aggregatesNetworkTrafficOdd() {
+        File file = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "trafficPattern01Odd.csv");
         Records records = new Records(file);
         List<Integer> periods = records.aggregatesNetworkTraffic(200,2);
 
         assertThat(periods)
-               .hasSize(9)
-               .containsExactly(720,453,1718,1025,883,279,1104,441,479);
+                .hasSize(9)
+                .containsExactly(720,453,1718,1025,883,279,1104,441,479);
+
+    }
+
+    @Test
+    void aggregatesNetworkTrafficEven() {
+        File file = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "trafficPattern01Even.csv");
+        Records records = new Records(file);
+        List<Integer> periods = records.aggregatesNetworkTraffic(200,2);
+
+        assertThat(periods)
+                .hasSize(10)
+                .containsExactly(720,453,1718,1025,883,279,1104,441,479,750);
 
     }
 }
