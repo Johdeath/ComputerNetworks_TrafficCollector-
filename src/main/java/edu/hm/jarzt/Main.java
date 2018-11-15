a8fc89e3c163166a7e21c1e6a1257ef734141058
Nexus457 <maier14@hm.edu>
2018-11-15 09:57:45 +0100

    add gitignore

--- src/main/java/edu/hm/jarzt/Main.java
+++ src/main/java/edu/hm/jarzt/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        File file = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "trafficPattern01 - Kopie.csv");
+        File file = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "trafficPattern01.csv");
         Records records = new Records(file);
 
 
