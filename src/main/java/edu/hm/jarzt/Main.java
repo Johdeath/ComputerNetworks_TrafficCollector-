0cc29810051ddd3bf1daa7c32105456a02fbd410
Tom <weissbier.weissbier@googlemail.com>
2018-11-23 09:16:05 +0100

    added new fingerprint - big bug bunny ohne audio 1sec segments

--- src/main/java/edu/hm/jarzt/Main.java
+++ src/main/java/edu/hm/jarzt/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        File file = new File(System.getProperty("user.dir") + File.separator + "trafficPattern" + File.separator + "enp0s_NightSchoolAllinBytes.csv");
+        File file = new File(System.getProperty("user.dir") + File.separator + "trafficPattern" + File.separator + "bigbugbunny1.csv");
 
         File pathToJamesVideoFiles = new File(System.getProperty("user.dir") + File.separator + "videos" + File.separator +"james" + File.separator);
 
@@ -24,6 +24,15 @@ public class Main {
 
         Records records = new Records(file);
 
+        // BBB ohne audio
+        // Original 0.014655912957392868
+        // trailer 0.022610747772891644
+        // james 0.02210123016744051
+
+
+
+        // BIG bug bunny 0.03086809395955986
+        // james 0.024741583421318682
 
 
         //Utils.differential(periods).forEach(System.out::println);
@@ -39,15 +48,25 @@ public class Main {
         List<Long> list1 = Utils.getFileSizeInBytes(jamesVideoFiles);
         //List<Double> list2 = Utils.differential(list1);
         List<Long> listResult = Utils.generateFingerprint(list1, 6);
-        List<Double> listDiff = Utils.differential(listResult);
-        List<Double> fingerprint = new ArrayList<> (Utils.normalize(listDiff));
 
+
+        List<Double> listDiff = Utils.differential(listResult);
+
+        List<Double> fingerprint = new ArrayList<> (Utils.normalize(listDiff));
+        //fingerprint.forEach(System.out::println);
+
+
+        System.out.println("ASAaadsflkjasdfjklöasdljkfsajklfdasjklfjkldöfjklödf");
 
         //ToDo move into method in Utils
         // Traffic pattern CSV - File
         List<Long> periods = records.aggregatesNetworkTraffic(20000, 6);
+
+
         List<Double> pattern = Utils.differential(periods);
+
         List<Double> trafficPattern = new ArrayList<> (Utils.normalize(pattern));
+        //trafficPattern.forEach(System.out::println);
 
         System.out.println(Utils.pdtw(fingerprint,trafficPattern));
 
